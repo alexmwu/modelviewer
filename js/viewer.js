a6cf8f51dfccfa931bcb9662de4bee995f2b6084
Alex Wu <alexmwuwork@gmail.com>
2015-06-17 00:32:54 -0500

    add comments for better readability

--- js/viewer.js
+++ js/viewer.js
@@ -9,16 +9,19 @@ var eye;
 var vMatrix;    //view matrix
 var pMatrix;    //projection matrix
 
+//initialize view matrix variables, projection matrix variables, and shaders
 function init(){
+    //vars for view matrix
     theta=0;
     phi=0;
     radius=1;
+
     pMatrix=perspective(60,canvas.width/canvas.height,0.01,10.0);
     initColorShaders();
     resetView();
 }
 
-
+//reset the view matrix (vMatrix) and draw scene based on vMatrix
 function resetView(){
     var cartCoords=this.polarToCartesian();
     eye=vec3(cartCoords[0],cartCoords[1],cartCoords[2]);
@@ -55,28 +58,42 @@ var down=vec3(0.0,1.0,0.0);
     drawScene();
 }
 
+//render each model object
 function drawScene(){
     for(var i=0;i<models.length;i++){
         models[i].render();
     }
 }
 
-
+//initialize color shaders
 function initColorShaders(){
+    //get shaders for gl from script tags (named color-vShader, fShader)
     colorShaderProgram=initShaders(gl,"color-vShader","color-fShader");
-    
+   
+    //not sure why use this.colorProgram rather than colorShaderProgram
     gl.useProgram(this.colorProgram);
 
+    //get location of vPosition attr in shader and enable it
     colorShaderProgram.vPositionAttribute=gl.getAttribLocation(colorShaderProgram,"vPosition");
     gl.enableVertexAttribArray(colorShaderProgram.vPositionAttribute);
     
+    //get location of vColor attr in shader and enable it
     colorShaderProgram.vColorAttribute=gl.getAttribLocation(colorShaderProgram,"vColor");
     gl.enableVertexAttribArray(colorShaderProgram.vColorAttribute);
 
+    //get locations of uniform variables in shaders
     colorShaderProgram.pMatrixUniform=gl.getUniformLocation(colorShaderProgram,"pMatrix");
     colorShaderProgram.mvMatrixUniform=gl.getUniformLocation(colorShaderProgram,"mvMatrix"); 
 }
 
+//initialize the non-color shaders
+function initGrayShaders(){}
+
+function useProgram(){
+    gl.useProgram
+}
+
+//returns array of cartesian coordinates based on theta and phi
 function polarToCartesian(){
     var cartCoords=[];
     var xPos=radius*Math.sin(theta*Math.PI/360)*Math.cos(phi*Math.PI/360);
