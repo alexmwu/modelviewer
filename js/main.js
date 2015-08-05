2fc39b651167ceb1fe40c3968ac51a4500461c95
Alex Wu <alexmwuwork@gmail.com>
2015-08-05 09:33:52 -0500

    Finish clean up of main.js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
-//some globals that get loaded immediately
+// some globals that get loaded immediately
 var gl;
 var canvas;
-var wireframe,vertColoring;     //vertColoring true means colored using obj file (i.e., not texture data, false means flat shading
-var models; //array of loaded models
-var currentModel;   //current model focus (e.g., which model to manipulate)
+var wireframe, vertColoring;     // vertColoring true means colored using obj file (i.e., not texture data, false means flat shading
+var models; // array of loaded models
+var currentModel;   // current model focus (e.g., which model to manipulate)
 
-window.onload=function(){
+window.onload = function() {
 	// Check for the various File API support.
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
 	  // Great success! All the File APIs are supported.
-	  //listen for new file chosen
-	  var className=document.getElementsByClassName("file-picker");
-	  //add a listener for each of the class
-	  for(var i=0;i<className.length;i++){
-	  		className[i].addEventListener('change',readSingleFile,false);
+	  // listen for new file chosen
+	  var className = document.getElementsByClassName("file-picker");
+	  // add a listener for each of the class
+	  for(var i = 0; i < className.length; i++) {
+	  		className[i].addEventListener('change', readSingleFile, false);
 	  }
 
 	} else {
 	  alert('The File APIs are not fully supported by this browser. This application may not be able to load files.');
 	}
- 
-    models=[];
 
-    //if it is auto checked, make wireframe false, else true (it is a global)
-    wireframe=document.getElementById("wireframe").checked==true;
-    color=document.getElementById("colored").checked==true;
+    models = [];
 
-    //init GL canvas
-    canvas=document.getElementById("gl-canvas");
-    gl=WebGLUtils.setupWebGL(canvas);
-    if(!gl){
+    // if it is auto checked, make wireframe false, else true (it is a global)
+    wireframe = document.getElementById("wireframe").checked == true;
+    color = document.getElementById("colored").checked == true;
+
+    // init GL canvas
+    canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if(!gl) {
         alert("WebGL isn't available");
     }
 
 
-    //configure WebGL
-    gl.viewport(0,0,canvas.width,canvas.height);
+    // configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height);
     init();
 };
 
-function addModel(model){
+function addModel(model) {
     models.push(model);
-    currentModel=model;
+    currentModel = model;
 }
 
 
-function switchWireframe(){
-    wireframe=!wireframe;
+function switchWireframe() {
+    wireframe = !wireframe;
     drawScene();
 }
