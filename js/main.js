9f0bc37c28b3b2bd801b95dbcd08369a351ba62c
Alex Wu <alexmwuwork@gmail.com>
2015-08-05 09:37:45 -0500

    Properly indent main.js

--- js/main.js
+++ js/main.js
@@ -6,46 +6,46 @@ var models; // array of loaded models
 var currentModel;   // current model focus (e.g., which model to manipulate)
 
 window.onload = function() {
-	// Check for the various File API support.
-	if (window.File && window.FileReader && window.FileList && window.Blob) {
-	  // Great success! All the File APIs are supported.
-	  // listen for new file chosen
-	  var className = document.getElementsByClassName("file-picker");
-	  // add a listener for each of the class
-	  for(var i = 0; i < className.length; i++) {
-	  		className[i].addEventListener('change', readSingleFile, false);
-	  }
-
-	} else {
-	  alert('The File APIs are not fully supported by this browser. This application may not be able to load files.');
-	}
-
-    models = [];
-
-    // if it is auto checked, make wireframe false, else true (it is a global)
-    wireframe = document.getElementById("wireframe").checked == true;
-    color = document.getElementById("colored").checked == true;
-
-    // init GL canvas
-    canvas = document.getElementById("gl-canvas");
-    gl = WebGLUtils.setupWebGL(canvas);
-    if(!gl) {
-        alert("WebGL isn't available");
+  // Check for the various File API support.
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // Great success! All the File APIs are supported.
+    // listen for new file chosen
+    var className = document.getElementsByClassName("file-picker");
+    // add a listener for each of the class
+    for(var i = 0; i < className.length; i++) {
+      className[i].addEventListener('change', readSingleFile, false);
     }
 
+  } else {
+    alert('The File APIs are not fully supported by this browser. This application may not be able to load files.');
+  }
 
-    // configure WebGL
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    init();
+  models = [];
+
+  // if it is auto checked, make wireframe false, else true (it is a global)
+  wireframe = document.getElementById("wireframe").checked == true;
+  color = document.getElementById("colored").checked == true;
+
+  // init GL canvas
+  canvas = document.getElementById("gl-canvas");
+  gl = WebGLUtils.setupWebGL(canvas);
+  if(!gl) {
+    alert("WebGL isn't available");
+  }
+
+
+  // configure WebGL
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  init();
 };
 
 function addModel(model) {
-    models.push(model);
-    currentModel = model;
+  models.push(model);
+  currentModel = model;
 }
 
 
 function switchWireframe() {
-    wireframe = !wireframe;
-    drawScene();
+  wireframe = !wireframe;
+  drawScene();
 }
