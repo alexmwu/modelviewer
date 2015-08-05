24edc80ddc9f0129f398425367f188ddbe27e9bb
Alex Wu <alexmwuwork@gmail.com>
2015-08-05 09:22:15 -0500

    Finish clean up of ajaxfile.js

--- js/ajaxfile.js
+++ js/ajaxfile.js
@@ -1,29 +1,29 @@
-window.onload=function(){
-  document.getElementById("submiturl").onclick = function(){
-    var url=document.getElementById('objurl').value;
-    var resp=readFileFromURL(url);
-    document.getElementById('obj').innerHTML=resp;
+window.onload = function() {
+  document.getElementById("submiturl").onclick  =  function() {
+    var url = document.getElementById('objurl').value;
+    var resp = readFileFromURL(url);
+    document.getElementById('obj').innerHTML = resp;
   }
 }
 
 // reads text from file given a URL and return the response
-function readFileFromURL(url){
+function readFileFromURL(url) {
   console.log("URL: "+url);
-  var xhr=CORSRequest("GET",url);
+  var xhr = CORSRequest("GET",url);
 
-  xhr.onerror=function(){
+  xhr.onerror = function() {
     return -3;
   }
 
   xhr.send();
 
-  if(xhr==null){
+  if(xhr == null) {
     return -1;
   }
-  if(xhr.readyState=4){
+  if(xhr.readyState = 4) {
     console.log(xhr.responseText+"\n"+xhr.getResponseHeader('content-type')+"\n"+xhr.status);
-    if(xhr.status==200){
-      var resp=json.parse(xhr.responsetext);
+    if(xhr.status == 200) {
+      var resp = json.parse(xhr.responsetext);
       return resp;
     }
     else{
@@ -34,35 +34,35 @@ function readFileFromURL(url){
 
 }
 
-function CORSRequest(method,url){
+function CORSRequest(method,url) {
   var xhr = new XMLHttpRequest();
-  if("withCredentials" in xhr){
+  if("withCredentials" in xhr) {
     xhr.open(method,url,true);
   }
-  else if(typeof XDomainRequest!="undefined"){
-    xhr=new XDomainRequest();
+  else if(typeof XDomainRequest != "undefined") {
+    xhr = new XDomainRequest();
     xhr.open(method,url);
   }
   else{
-    xhr=null;
+    xhr = null;
   }
   return xhr;
 }
 
-function printText(){
-  var url=document.getElementById('objurl').value;
-  var resp=readFileFromURL(url);
+function printText() {
+  var url = document.getElementById('objurl').value;
+  var resp = readFileFromURL(url);
 
-  if(resp==-1){
-    resp="CORS is not supported";
+  if(resp == -1) {
+    resp = "CORS is not supported";
   }
-  else if(resp==-2){
-    resp="URL error";
+  else if(resp == -2) {
+    resp = "URL error";
   }
-  else if(resp==-3){
-    resp="Network error";
+  else if(resp == -3) {
+    resp = "Network error";
   }
   console.log(resp);
-  document.getElementById('obj').innerHTML=resp;
-  event.preventDefault ? event.preventDefault() : event.returnValue=false;
+  document.getElementById('obj').innerHTML = resp;
+  event.preventDefault ? event.preventDefault() : event.returnValue = false;
 }
